e17ebacb0ab50c46604db05fca1a0d9015b66a1f
Puneet Shukla <106764262+puneetshukla14@users.noreply.github.com>
2025-09-03 09:08:42 +0530

    updation of page .tsx

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,9 @@
+"use client";
+
+export default function Dashboard() {
+  return (
+    <div className="flex h-screen bg-white text-black items-center justify-center">
+      <h1 className="text-3xl font-bold">Hello</h1>
+    </div>
+  );
+}
